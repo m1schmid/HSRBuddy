1a035f893eb32f278b9309917382ada6dca919c9
Mike Schmid <m1schmid@hsr.ch>
2012-10-23 17:34:38 +0200

    improved dates activity

--- HSRBuddy/src/ch/hsr/hsrbuddy/DatesActivity.java
+++ HSRBuddy/src/ch/hsr/hsrbuddy/DatesActivity.java
@@ -9,7 +9,10 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Html;
 import android.view.Menu;
 import android.widget.LinearLayout;
@@ -19,51 +22,74 @@ public class DatesActivity extends Activity {
 
 	private ArrayList<String> semesterDateURLs = new ArrayList<String>();
 	private ArrayList<String> dates = new ArrayList<String>();
+	final Handler datesHandler = new Handler();
+	private LinearLayout layout;
+	private Context that = this;
+	private ProgressDialog mDialog;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_dates);
-		LinearLayout layout = (LinearLayout) findViewById(R.id.dates);
-
-		getURLsToParse();
-		
-		for (int i = 0; i < semesterDateURLs.size(); i++) {
-			
-		}
-
-		Document doc;
-		try {
-			doc = Jsoup.connect(semesterDateURLs.get(0)).get(); //TODO: add the second view
-			Elements rows = doc.select("table tr");
-			for (int i = 1; i < rows.size(); i++) {
-				Elements cells  = rows.get(i).select("td");
-				if(cells.get(0).select("b").isEmpty() && cells.get(0).select("[style*=bold]").isEmpty()){
-					if(cells.get(0).text().length()>0){
-						dates.add(cells.get(0).text() +": " + cells.get(1).text() );						
-					} else {
-						dates.add("-------------------");
-					}
-				} else {
-					dates.add("<b>" + cells.get(0).text() + "<b>");
-				}
-			}	
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		TextView informationView;
-
-		for (int i = 0; i < dates.size()-3; i++) {
-			informationView = new TextView(this);
-			informationView.setText(Html.fromHtml(dates.get(i)));
-			layout.addView(informationView, i);
-
-		}
-
+		layout = (LinearLayout) findViewById(R.id.dates);
 	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mDialog = new ProgressDialog(this);
+		mDialog.setMessage("Loading...");
+        mDialog.setCancelable(false);
+        mDialog.show();
+
+		new Thread(parseValues, "parseValuesThread").start();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_dates, menu);
+		return true;
+	}
+
+	private final Runnable parseValues = new Runnable() {
+		public void run() {
+			getURLsToParse();
+			Document doc;
+			try {
+				doc = Jsoup.connect(semesterDateURLs.get(0)).get(); // TODO: add the second view
+				Elements rows = doc.select("table tr");
+				for (int i = 1; i < rows.size(); i++) {
+					Elements cells = rows.get(i).select("td");
+					if (cells.get(0).select("b").isEmpty() && cells.get(0).select("[style*=bold]").isEmpty()) {
+						if (cells.get(0).text().length() > 0) { 
+							dates.add(cells.get(0).text() + ": " + cells.get(1).text());
+						} else {
+							dates.add("-------------------");
+						}
+					} else {
+						dates.add("<b>" + cells.get(0).text() + "<b>");
+					}
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			datesHandler.post(updateUI);
+		}
+	};
+
+	final Runnable updateUI = new Runnable() {
+		public void run() {
+			TextView informationView;
+
+			for (int i = 0; i < dates.size() - 3; i++) {
+				informationView = new TextView(that);
+				informationView.setText(Html.fromHtml(dates.get(i)));
+				layout.addView(informationView, i);
+			}
+			mDialog.dismiss();
+		}
+	};
 
 	private void getURLsToParse() {
 		Document doc;
@@ -82,11 +108,5 @@ public class DatesActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_dates, menu);
-		return true;
 	}
 }
