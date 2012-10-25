f2b2f655b726f9368482a3367ed1a8e2676eac6c
dwirz <dwirz@hsr.ch>
2012-10-25 13:34:55 +0200

    added map functionality

--- HSRBuddy/src/ch/hsr/hsrbuddy/activity/MapActivity.java
+++ HSRBuddy/src/ch/hsr/hsrbuddy/activity/MapActivity.java
@@ -1,23 +1,53 @@
 package ch.hsr.hsrbuddy.activity;
 
-import ch.hsr.hsrbuddy.R;
-import ch.hsr.hsrbuddy.R.layout;
-import ch.hsr.hsrbuddy.R.menu;
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.view.Menu;
+import ch.hsr.hsrbuddy.R;
+import ch.hsr.hsrbuddy.view.ImageMap;
+import ch.hsr.hsrbuddy.view.ImageMap.OnImageMapClickedHandler;
 
 public class MapActivity extends Activity {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_map, menu);
-        return true;
-    }
+	private boolean isDetailMap = false;
+	ImageMap mImageMap;
+	Drawable hsrmap;
+	
+	OnImageMapClickedHandler handler = new ImageMap.OnImageMapClickedHandler() {
+		public void onImageMapClicked(int id) { changeMap(id); }
+		public void onBubbleClicked(int id) { }
+	};
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map);
+		mImageMap = (ImageMap) findViewById(R.id.map);
+		mImageMap.addOnImageMapClickedHandler(handler);
+	}
+	
+	private void changeMap(int id){
+		if(id == R.id.building1){
+			mImageMap.setImageResource(R.drawable.detail_building_1);
+			mImageMap.removeOnImageMapClickedHandler(handler);
+			isDetailMap = true;
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		if(isDetailMap){
+			mImageMap.setImageResource(R.drawable.hsrmap);
+			mImageMap.addOnImageMapClickedHandler(handler);
+			isDetailMap = false;
+		}else{
+			super.onBackPressed();
+		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_map, menu);
+		return true;
+	}
 }
