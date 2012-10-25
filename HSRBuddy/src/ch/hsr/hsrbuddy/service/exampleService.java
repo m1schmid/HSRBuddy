311545ac49f0925076f30a96e399d288630a8d96
Mike Schmid <m1schmid@hsr.ch>
2012-10-25 16:17:45 +0200

    -.-

---
+++ HSRBuddy/src/ch/hsr/hsrbuddy/service/exampleService.java
@@ -0,0 +1,32 @@
+package ch.hsr.hsrbuddy.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+
+
+public class exampleService extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO Auto-generated method stub
+		return super.onStartCommand(intent, flags, startId);
+	}
+	
+	
+
+
+}
