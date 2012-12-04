02c42bc6ebc27094672c4809eff3d5e88c9ecafc
dwirz <dominique.wirz@gmail.com>
2012-12-04 21:25:00 +0100

    cleaned code

--- HSRBuddy/src/ch/hsr/hsrbuddy/activity/MainActivity.java
+++ HSRBuddy/src/ch/hsr/hsrbuddy/activity/MainActivity.java
@@ -11,7 +11,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 import ch.hsr.hsrbuddy.R;
 import ch.hsr.hsrbuddy.activity.map.MapActivity;
 
